b399ad03903a0914c66231089605312e9869d249
Heli Rosales <helirosalesvillanueva@gmail.com>
2022-01-11 16:22:52 -0300

    Se agrego mi nombre à la pagina

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,14 @@ function Header(){
 
 
     return(
+        <>
             <h1 className="font-black text-5xl text-center w-1/2 mx-auto">Seguimiento pacientes {""}
-            <span className="text-indigo-600">Veterinaria</span>
+                <span className="text-indigo-600">Veterinaria</span>
             </h1>
+            <h3 className="font-medium text-right mx-5 ">
+                Pagina hecha por: Heli Rosales
+            </h3>
+        </>
     )
 }
 
